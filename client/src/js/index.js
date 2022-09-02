fd6c289a60200de09f0fd37caf1dba3d20d7b991
Joseph Kurpierz <josephkurpierz@gmail.com>
2022-09-02 13:17:46 -0500

    html render

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,7 +1,6 @@
 //import modules
 import  "./form";
-import "./submit";
-
+import {toggleForm, clearForm} from './form'
 //import images
 import Logo from '../images/logo.png';
 import Bear from '../images/bear.png';
@@ -14,12 +13,52 @@ import { Tooltip, Toast, Popover } from 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 //import indexed db
-import {initdb} from './database';
+import { initDb, getDb, postDb } from './database';
+
+//import html render
+import {fetchCards} from './cards';
 
 //add images on load
 window.addEventListener('load', function () {
-  initdb();
+  initDb();
+  fetchCards();
   document.getElementById('logo').src = Logo;
   document.getElementById('bearThumbnail').src = Bear;
   document.getElementById('dogThumbnail').src = Dog;
 });
+
+  // Form functionality
+  const form = document.getElementById("formToggle");
+  const newContactButton = document.getElementById("new-contact");
+  let submitBtnToUpdate = false;
+  let profileId;
+  
+  newContactButton.addEventListener('click', event => {
+    toggleForm()
+   })
+  
+  form.addEventListener('submit', event => {
+    // Handle data
+    event.preventDefault();
+  let name = document.getElementById("name").value;
+  let phone = document.getElementById("phone").value;
+  let email = document.getElementById("email").value;
+  let profile = document.querySelector('input[type="radio"]:checked').value;
+  
+    // Post form data to IndexedDB OR Edit an existing card in IndexedDB
+  if (submitBtnToUpdate == false) {
+    postDb(name, email, phone, profile);
+  } else {
+  
+    fetchCards();
+      // Toggles the submit button back to POST functionality
+    submitBtnToUpdate = false;
+  }
+  
+  // Clear form
+  clearForm();
+  // Toggle form
+  toggleForm();
+  // Reload the DOM
+  fetchCards();
+  });
